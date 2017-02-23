bc9ab7e2d0bdba8eab5eb665f44eb46f874a91ba
Alexey Shobanov <shobanov@gmail.com>
2017-02-23 15:31:11 +0300

    The sixth stage of the first level. Edit #7

--- src/games/brainBalance.js
+++
@@ -1,9 +0,0 @@
-import { cons, car, cdr, toString } from 'hexlet-pairs';
-import { computeRandomInteger, runSelectedGame } from '../commonFunction';
-
-const findBalanceNum = (num) => {
-  const strNum = String(num);
-  strNum.sort();
-
-
-}
