2b34ce7d18fa45a18902b6110998ec448d484127
Alexey Shobanov <shobanov@gmail.com>
2017-02-24 23:01:19 +0300

    The ninth stage of the first level

--- src/games/Progression.js
+++ src/games/Progression.js
@@ -1,36 +1,35 @@
+
 import { computeRandomInteger, runSelectedGame } from '../commonFunction';
 
 const makeArithmeticalProgression = (dimension) => {
   const firstNum = computeRandomInteger(1, 100);
   const step = computeRandomInteger(1, 100);
-  const iter = (currentNum, arithmeticalProgression) => {
-    const sizeMap = arithmeticalProgression.size;
-    if (sizeMap === dimension) {
-      return arithmeticalProgression;
+  const iter = (num, count, acc) => {
+    if (count === 1) {
+      return acc;
     }
-    return iter(currentNum + step, arithmeticalProgression.set(sizeMap, currentNum));
+    const newAcc = [...acc, num];
+    return iter(num + step, count - 1, newAcc);
   };
-  return iter(firstNum, new Map());
+  return iter(firstNum, dimension, []);
 };
 
 const makeMissInArithmeticalProgression = (dimension) => {
   const arithmeticalProgression = makeArithmeticalProgression(dimension);
-  const passPosission = computeRandomInteger(1, 10);
-  const passNum = arithmeticalProgression.get(passPosission);
-  arithmeticalProgression.set('pass', passNum);
-  arithmeticalProgression.set(passPosission, '..');
-  return arithmeticalProgression;
+  const passPosission = computeRandomInteger(1, dimension - 1);
+  return [passPosission, ...arithmeticalProgression];
 };
+
+const task = 'What number is missing in this progression?';
 
 const runBrainProgression = () => {
   const dimension = 10;
-  const task = 'What number is missing in this progression?';
   const question = (randomData) => {
-    const progressionForPrint = [...randomData.values()];
-    progressionForPrint.pop();
-    return progressionForPrint.join(' ');
+    const [passPosission, ...arithmeticalProgression] = [...randomData];
+    const passNum = arithmeticalProgression[passPosission];
+    return arithmeticalProgression.join(' ').replace(String(passNum), '..');
   };
-  const getResult = randomData => randomData.get('pass');
+  const getResult = randomData => randomData[randomData[0] + 1];
   const randomData = () => makeMissInArithmeticalProgression(dimension);
   runSelectedGame(task, randomData, question, getResult);
 };
